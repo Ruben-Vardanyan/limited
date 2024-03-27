14db4ccfe82087e3c82952edf52465517f145147
Ruben Vardanyan <53569596+Ruben-Vardanyan@users.noreply.github.com>
2024-03-27 09:21:51 +0400

    Update xnd3.php

--- toms10/xnd3.php
+++ toms10/xnd3.php
@@ -1,42 +1,27 @@
 <?php 
-    session_start();
-    $conn = mysqli_connect("localhost","root","","xnd");
+    $conn = mysqli_connect("localhost","root","","test");
 
     if(!$conn){
-        die("died");
+        die("died: " . $conn->connect_error());
     }
 
-    $_SESSION['fullname'] = isset($_SESSION['fullname']) ? $_SESSION['fullname'] : "";
-    $_SESSION['university'] = isset($_SESSION['university']) ? $_SESSION['university'] : "HAPH";
-    $_SESSION['birthyear'] = isset($_SESSION['birthyear']) ? $_SESSION['birthyear'] : "";
-
-
     if($_SERVER['REQUEST_METHOD'] == "POST"){
-        $fullname = $_SESSION['fullname'] = $_POST['fullname'];
-        $university = $_SESSION['university'] = $_POST['university'];
-        $birthyear = $_SESSION['birthyear'] = $_POST['birthyear'];
-
-        $conn->query("INSERT INTO `student`(`fullname`, `university`, `birthyear`) 
-        VALUES ('$fullname','$university','$birthyear')");
-
-        header("Location: xnd3.php");
-        exit();
-    } 
-    $conn->close();
-?>
-
-<form action="" method="post">
-    fullname
-    <input type="text" name="fullname" value="<?=$_SESSION['fullname']?>" required><br>
-    university
-    <input type="text" name="university" value="<?=$_SESSION['university']?>" required><br>
-    birthdate
-    <select name="birthyear"  required>
-        <?php
-            foreach(range(2024,1900,-1) as $year){
-                echo "<option value='$year' ".($_SESSION['birthyear'] == $year ? "selected" : "" ).">$year</option>";
+        if(isset($_POST["fullname"]) && isset($_POST["birthyear"])){
+            $sql = "INSERT INTO `student`(`fullname`, `birthyear`) 
+            VALUES ('{$_POST['fullname']}','{$_POST['birthyear']}')";
+            
+            if($conn->query($sql)){
+                echo "the student added";
             }
-        ?>
-    </select><br>
+        }
+
+        $conn->close();
+    }
+?>
+<form action="" method="post">
+    fullname <br>
+    <input type="text" name="fullname" id=""> <br>
+    birthyear <br>
+    <input type="number" name="birthyear" min="1900" max="2024" id=""> <br>
     <input type="submit" value="insert">
 </form>
