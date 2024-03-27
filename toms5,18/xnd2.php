7e337f5e72de336a403984b0eaa12c6afbdedcdf
Ruben Vardanyan <53569596+Ruben-Vardanyan@users.noreply.github.com>
2024-03-27 09:24:29 +0400

    Update xnd2.php

--- toms5,18/xnd2.php
+++ toms5,18/xnd2.php
@@ -1,31 +1,16 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="" method="post">
-        name <br>
-        <input type="text" name="name" required />
-        <br>
-        male
-        <input type="radio" name="gender" required value="male">
-        female
-        <input type="radio" name="gender" required value="female">
-        <br>
-        <br>
-        <input type="submit">
-    </form>
-
-    <?php 
-        if($_SERVER['REQUEST_METHOD'] == "POST"){
-            echo "<h1>{$_POST['name']}</h1>";
-            echo "<h3>{$_POST['gender']}</h3>";
+<form action="" method="post">
+    name <br>
+    <input type="text" name="name"><br>
+    male <input type="radio" name="gender" value="male">
+    female <input type="radio" name="gender" value="female">
+    <br>
+    <input type="submit" value="add">
+</form>
+<?php 
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        if(isset($_POST['name']) && isset($_POST['gender'])){
+            echo $_POST['name'] . "<br>";
+            echo $_POST['gender'] . "<br>";
         }
-    ?>
-</body>
-</html>
+    }
+?>
